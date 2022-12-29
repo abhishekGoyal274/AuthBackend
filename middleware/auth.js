cc55f55a2a65800fbfd5885b2cf4f59b84a45bc8
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2022-12-29 14:47:05 +0530

    backend completed for local environment and working, will change for uploading

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 function auth(req, res, next) {
     try {
-        if (!req.cookies.token) return req.status(401).json({ errorMessage: "Unautherised" });
+        // console.log(req.cookies.token);
+        if (!req.cookies.token) return res.status(401).json({ errorMessage: "Unautherised" });
         const verified = jwt.verify(req.cookies.token, process.env.JWT_SECRET)
         req.userId = verified.user;
         next();
