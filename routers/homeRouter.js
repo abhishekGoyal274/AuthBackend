b97168928bcb3641e885b1eeb2f013677f887905
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2022-12-29 18:09:00 +0530

    edited favicon

--- routers/homeRouter.js
+++ routers/homeRouter.js
@@ -5,7 +5,7 @@ var ApiHomeHTML = '\
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>\
         <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">\
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">\
-        <link rel="icon" type="image/x-icon" href="https://puchd.ac.in/asset/pu-logo.png">\
+        <link rel="icon" type="image/x-icon" href="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png">\
         <title>API PuMeet</title>\
             <body style="background-color:black; margin:0px; padding:0px; font-family: Josefin Sans, sans-serif;;\">\
                 <div style="display: flex;\
