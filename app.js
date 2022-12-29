a97f752461e4a0a7b06d6d611adc2825cabae637
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2022-12-29 17:55:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:3000", "https://auth-login.netlify.app/"],
+  origin: ["http://localhost:3000", "https://auth-login.netlify.app"],
   credentials:true,
 }));
 
